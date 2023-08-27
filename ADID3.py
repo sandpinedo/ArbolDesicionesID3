c2cc77ea7d23ddedd74d9d0278cf8b0e708d8dff
sandpinedo <1440690@senati.pe>
2023-08-27 10:01:13 -0500

    Ingrese los datos de clima y Temperatura

--- ADID3.py
+++ ADID3.py
@@ -1,3 +1,11 @@
 from collections import Counter
 import math
 
+# Datos del Clima y Temperatura
+data = [
+    ["soleado", "alta", "no"],
+    ["soleado", "suave", "no"],
+    ["nublado", "alta", "si"],
+    ["lluvioso", "alta", "si"],
+    ["lluvioso", "suave", "si"],
+]   
